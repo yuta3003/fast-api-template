183101aa049df3564d1fc68de26b0aa6a567b537
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-11-16 17:27:57 +0900

    :+1: update

--- FastAPI/api/main.py
+++ FastAPI/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from api.routers import tasks, done
+from api.routers import task, done
 
 app = FastAPI()
-app.include_router(tasks.router)
+app.include_router(task.router)
 app.include_router(done.router)
